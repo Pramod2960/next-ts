96777484a1536df3c71721cb09b2d3b6331c316d
pramod <pramodkumar2960.com>
2024-02-01 13:15:58 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+   <div>
+    <div className="container"></div>
+    <h1 className="text-red">Hello</h1>
+    <h2 className="font-bold 2xl">Word</h2>
+
+   </div>
+  );
+}
